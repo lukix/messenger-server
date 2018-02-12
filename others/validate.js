49d791f186f9850919bda08cae0b8c5de825e1ee
lukix <lukix.95@gmail.com>
2018-02-13 00:19:55 +0100

    Add optional props to the validation function

--- others/validate.js
+++ others/validate.js
@@ -1,11 +1,15 @@
-module.exports = function validate(object, requiredProps, allowExtraProps = false) {
+function validate(object, requiredProps, optionalProps = [], allowExtraProps = false) {
+	const allowedPropsNames = [...requiredProps, ...optionalProps]
 	const extraPropsError = allowExtraProps
 		? null
-		: Object.keys(object).length <= requiredProps.length
+		: Object.keys(object)
+			.every(key => allowedPropsNames.find(({ name }) => name === key) !== undefined)
 			? null
 			: 'Not expected property found'
 	const errorMessages = [
-		...requiredProps.map(checkProperty(object)),
+		...requiredProps.map(checkPropertyExistance(object)),
+		...requiredProps.map(checkPropertyType(object)),
+		...optionalProps.map(checkPropertyType(object)),
 		extraPropsError,
 	].filter(errorMessage => errorMessage !== null)
 	
@@ -15,11 +19,18 @@ module.exports = function validate(object, requiredProps, allowExtraProps = fals
 	}
 }
 
-const checkProperty = (object) => ({ name, type }) => {
-	const actualType = typeof object[name]
+const checkPropertyExistance = (object) => ({ name }) => {
 	return !object.hasOwnProperty(name)
 		? `Required property '${name}' is missing`
+		: null
+}
+const checkPropertyType = (object) => ({ name, type }) => {
+	const actualType = typeof object[name]
+	return !object.hasOwnProperty(name)
+		? null
 		: actualType !== type && type !== 'any' && type !== undefined
 			? `Invalid type ${actualType} of ${name}. Expected ${type}`
 			: null
 }
+
+module.exports = validate
