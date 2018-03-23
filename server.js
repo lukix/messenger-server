72abf00de534ddf88c403eb4668cc8d5e07b3b32
lukix <lukix.95@gmail.com>
2018-03-23 23:58:57 +0100

    Separate static files serving from http API

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const path = require('path')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const sockets = require('./others/sockets')
 const messagesRouter = require('./routes/messagesRouter')
 const config = require('./config')
@@ -8,16 +8,12 @@ const config = require('./config')
 const app = express()
 const apiPath = 'api'
 
+app.use(cors())
 app.use(bodyParser.json())      		// to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 	extended: true,
 }))
 app.use(`/${apiPath}/messages`, messagesRouter)
-
-app.use(express.static('./build'))
-app.get('/*', (req,res) => {
-	res.sendFile(path.join(__dirname, '/../build/index.html'))
-})
 
 const server = app.listen(config.port, function () {
 	console.log('App is running on port: ' + config.port)
